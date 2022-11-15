46d9bbbaee41f43e4d883313c1a8ea855d5d41a6
Awab Osman <awabosman3@gmail.com>
2022-11-14 21:45:10 -0500

    complete router with crud operations for grocery list

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,43 @@
-var express = require('express');
-var router = express.Router();
+const express = require("express");
+const router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+module.exports = (db) => {
+  // GET /api/groceries
+  router.get("/groceries", (req, res) => {
+    db.query(`SELECT * FROM groceries;`)
+      .then(data => {
+        res.json(data.rows)
+      })
+      .catch (err => console.log(err));
+  })
 
-module.exports = router;
+  // PUT /api/groceries/:groceries_id
+  router.put("/groceries/:grocery_id", (req, res) => {
+    const item = req.body.item
+    const id = req.params.grocery_id
+    db.query(
+      `UPDATE groceries SET item = '$1' WHERE id = $2;`,
+      [item, id]
+    )
+  })
+
+  // POST /api/groceries
+  router.post("/groceries", (req, res) => {
+    const item = req.body.item
+    db.query(
+      `INSERT INTO groceries (item) VALUES($1);`,
+      [item]
+    )      
+  })
+
+  // DELETE /api/groceries
+  router.delete("/groceries/:grocery_id", (req, res) => {
+    const id = req.params.grocery_id
+    db.query(
+      `DELETE FROM groceries WHERE id = $1;`,
+      [id]
+    )
+  })
+
+  return router;
+};
