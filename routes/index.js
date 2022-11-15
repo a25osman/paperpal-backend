e1b660965035ac048abc5b5a063d17a29a06ff28
Awab Osman <awabosman3@gmail.com>
2022-11-15 17:03:27 -0500

    fix delete and update routes

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ const router = express.Router();
 module.exports = (db) => {
   // GET /api/groceries
   router.get("/groceries", (req, res) => {
-    db.query(`SELECT * FROM groceries;`)
+    db.query(`SELECT * FROM groceries ORDER BY id DESC;`)
       .then(data => {
         res.json(data.rows)
       })
@@ -14,11 +14,11 @@ module.exports = (db) => {
   // PUT /api/groceries/:grocery_id
   router.put("/groceries/:grocery_id", (req, res) => {
     const updatedGroceryItem = req.body.item
-    const updatedQty = req.body.qty
+    const updatedQty = Math.floor(req.body.qty)
     const grocery_id = req.params.grocery_id
     db
       .query(
-        `UPDATE groceries SET item = $1, qty = $2 WHERE id = $3;`,
+        `UPDATE groceries SET item = $1, qty = $2 WHERE id = $3 RETURNING *;`,
         [updatedGroceryItem, updatedQty, grocery_id]
       )
       .then(data => res.status(201).json(data))
@@ -40,10 +40,13 @@ module.exports = (db) => {
   // DELETE /api/groceries/:grocery_id
   router.delete("/groceries/:grocery_id", (req, res) => {
     const grocery_id = req.params.grocery_id
-    db.query(
-      `DELETE FROM groceries WHERE id = $1;`,
-      [grocery_id]
-    )
+    db
+      .query (
+        `DELETE FROM groceries WHERE id = $1;`,
+        [grocery_id]
+      )
+      .then(data => res.status(201).json(data))
+      .catch(err => res.status(500).json(err));
   })
 
   return router;
