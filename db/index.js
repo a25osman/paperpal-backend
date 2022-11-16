a604d56237af3091e1c81b2a3a5773cb0cabaeb1
Awab Osman <awabosman3@gmail.com>
2022-11-16 18:57:30 -0500

    docker file

--- db/index.js
+++ db/index.js
@@ -2,7 +2,8 @@ require('dotenv').config();
 const { Client } = require('pg');
 
 const client = new Client({
-  connectionString: process.env.DATABASE_URL,
+  // connectionString: process.env.DATABASE_URL,
+  connectionString: "DATABASE_URL=postgres://afidgblrwwfhgm:cf54a880867850795413f0c500531e7207a5ae31b9b2f2d2ed0f3da0e41bdadc@ec2-3-209-39-2.compute-1.amazonaws.com:5432/d4vabis2627eff",
   ssl: {
     rejectUnauthorized: false
   }
